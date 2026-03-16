865d48fa820f4b2d1aeb1a58b6581c589bcd02b0
sigma Student <dishapatil9223@gmail.com>
2026-03-16 12:38:49 +0530

    all commited

--- src/assets/compoenents/HeroBanner.jsx
+++ src/assets/compoenents/HeroBanner.jsx
@@ -2,7 +2,7 @@ import "../compoenents/HeroBanner.css";
 function HeroBanner() {
     return (
         <section>
-            <h1>OFFER OFFER !.. BIG EDUCATION OFFER</h1>
+            <h1>OFFER OFFER !.. BIG EDUCATION OFFER By Disha Patil</h1>
             <p>Upgarde youself not by compare but by compare your last 10 min with present</p>
         </section>
     );
